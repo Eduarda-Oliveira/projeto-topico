cd7e92bf5bf3fe6e5311d4459c5e16150b7284f0
Eduarda-Oliveira <duda.oliveira.1503@gmail.com>
2022-08-22 20:13:09 -0300

    first commit

---
+++ src/main/java/br/gov/sp/fatec/demo/ProjetoTopicoApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTopicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTopicoApplication.class, args);
+	}
+
+}
